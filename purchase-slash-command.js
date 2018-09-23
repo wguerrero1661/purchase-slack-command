99a0a055bb92eafe04e10738db582f83e3815d7b
Walter Guerrero <wguerrero1661@gmail.com>
2018-09-22 18:19:00 -0700

    .bin file not working

--- purchase-slash-command.js
+++ purchase-slash-command.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app= express();
+const bodyParser = require("body-parser");
+
+// allow to access post request
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.post("/purchase" , async (req, res) => {
   console.log(req);
